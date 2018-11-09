ce3254a31e75d190be43f1f357539928910057f0
Steve Senviel <senviel@gmail.com>
2018-11-09 16:43:59 -0500

    Update test_blackjack_deck.py
    adding more updates and test cases for blackjack game

---
+++ test_blackjack_deck.py
@@ -0,0 +1,34 @@
+import unittest
+from blackjack import Deck, Card
+
+verbosity_level = 3;
+
+class TestDeck(unittest.TestCase):
+
+    def setUp(self):
+        self.myTestDeck = Deck()
+
+    def tearDown(self):
+        del self.myTestDeck;
+
+    def test_deck_Randomness(self):
+        tempDeck = self.myTestDeck
+        self.myTestDeck.shuffle_cards();
+        self.assertNotEqual(tempDeck, self.myTestDeck, "Deck has not been shuffled.")
+
+    def test_deck_size(self):
+        self.assertEqual(len(self.myTestDeck), 52, "Deck is not the correct size!!")
+
+
+
+
+
+
+
+
+
+# load the tests
+suite_of_tests = unittest.TestLoader().loadTestsFromTestCase(TestDeck)
+
+# run the tests
+unittest.TextTestRunner(verbosity=verbosity_level).run(suite_of_tests)
