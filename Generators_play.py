15b81e1bc9f455aaa69ba8e5f81cc2c26cedb05c
Steve Senviel <senviel@gmail.com>
2019-01-10 21:40:21 -0500

    Create Generators_play.py
    adding new file for hmwk assignment

---
+++ Generators_play.py
@@ -0,0 +1,14 @@
+"""
+        problem 1:
+            create a Generator that generates the squares of numbers up to some number 'N'
+            
+        problem 2:
+            create a generator that yields "n" random number between a low and high number 
+            
+        problem 3: 
+            use the iter() function to convert the string 'hello'  into an iterator.
+
+
+
+
+"""
