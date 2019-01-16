095b2f5f1d4eeaf768e4a8f69417c52897e620e5
Steve Senviel <senviel@gmail.com>
2019-01-15 21:14:13 -0500

    1_15_2019_updates
    tested the implementations of Hmwk problems 1 & 2.  still need to implement problem #3.

--- Generators_play.py
+++ Generators_play.py
@@ -1,14 +1,38 @@
 """
-        problem 1:
-            create a Generator that generates the squares of numbers up to some number 'N'
-            
-        problem 2:
-            create a generator that yields "n" random number between a low and high number 
-            
-        problem 3: 
-            use the iter() function to convert the string 'hello'  into an iterator.
+    problem 1:
+        create a Generator that generates the squares of numbers up to some number 'N'    
+    problem 2:
+        create a generator that yields "n" random number between a low and high number
+        NOTE: use random.randrange or random.randint 
+    problem 3: 
+        use the iter() function to convert the string 'hello'  into an iterator.
 
 
 
 
 """
+import random
+
+
+def square_generator(N):
+
+    i=0
+    while i < N:
+        yield i**2
+        i += 1
+    
+def random_generator(N, start, stop):
+    """
+    NOTE: need to figure out how to get unique random numbers
+    """    
+    i=0
+    while i < N:
+        yield random.randrange(start, stop)
+        i += 1
+        
+def convert_str_to_iter(string):
+    pass
+
+
+        
+        
